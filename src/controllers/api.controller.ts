aa44446b6c5246f17f470992a44e1eb491e2d6b4
SungYong Jang <jsy@sclab.io>
2023-05-03 19:51:37 +0900

    [api] add request parameter from sql

--- src/controllers/api.controller.ts
+++ src/controllers/api.controller.ts
@@ -1,14 +1,72 @@
 import { NextFunction, Request, Response } from 'express';
 import { QueryItem, DBPool } from '../config/index';
 import { PoolConnection } from 'mariadb';
+import { getPlaceHolders, hasSql, replaceString } from '@/utils/util';
+import { logger } from '@/utils/logger';
 
 class APIController {
+  mappingRequestData(query: string, queryData: any): string {
+    // data mapping
+    const paramKeys = getPlaceHolders(query);
+
+    if (paramKeys.length > 0) {
+      const valueObj = {};
+
+      let paramKey: string, reqData: any;
+      for (let i = 0; i < paramKeys.length; i++) {
+        paramKey = paramKeys[i];
+        reqData = queryData[paramKey];
+        if (reqData !== undefined && reqData !== null) {
+          valueObj[paramKey] = reqData;
+        }
+      }
+
+      console.log(queryData, valueObj, paramKeys);
+
+      // make final query
+      return replaceString(query, valueObj);
+    } else {
+      return query;
+    }
+  }
+
   public queryItem?: QueryItem;
   public index = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     let conn: PoolConnection;
+
+    if (!this.queryItem || !this.queryItem.query) {
+      res.writeHead(500, {
+        'Content-Type': 'application/json',
+      });
+      res.end(
+        JSON.stringify({
+          message: 'Query item empty',
+        }),
+      );
+      res.end('Query item empty');
+      return;
+    }
+
+    let sql = this.queryItem.query;
+
+    sql = this.mappingRequestData(sql, req.query);
+    // check sql injection
+    if (hasSql(sql)) {
+      logger.info(`SQL inject detect with final query data, ${sql}, ${this.queryItem.query}, ${this.queryItem.endPoint}`);
+      res.writeHead(400, {
+        'Content-Type': 'application/json',
+      });
+      res.end(
+        JSON.stringify({
+          message: 'SQL inject data detected.',
+        }),
+      );
+      return;
+    }
+
     try {
       conn = await DBPool.getConnection();
-      const rows = await conn.query(this.queryItem.query);
+      const rows = await conn.query(sql);
       res.writeHead(200, {
         'Content-Type': 'application/json',
       });
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,18 +1,46 @@
-import request from 'supertest';
-import App from '@/app';
-import IndexRoute from '@routes/index.route';
+import { getPlaceHolders, hasSql, replaceString } from '../utils/util';
+import APIController from '../controllers/api.controller';
+import { QueryType } from '../config';
 
-afterAll(async () => {
-  await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
-});
+describe('설정과 파라미터를 통해 sql을 만들어주는 기능 테스트', () => {
+  test('getPlaceHolders', () => {
+    const sql = "select ${field} from ${table} where name='${name}'";
+    const map = {
+      field: '*',
+      table: 'member',
+      name: '홍길동',
+    };
 
-describe('Testing Index', () => {
-  describe('[GET] /', () => {
-    it('response statusCode 200', () => {
-      const indexRoute = new IndexRoute();
-      const app = new App([indexRoute]);
+    const placeHolders = getPlaceHolders(sql);
+    expect(placeHolders).toEqual(['field', 'table', 'name']);
+  });
 
-      return request(app.getServer()).get(`${indexRoute.path}`).expect(200);
-    });
+  test('replaceString', () => {
+    const sql = "select ${field} from ${table} where name='${name}'";
+    const map = {
+      field: '*',
+      table: 'member',
+      name: '홍길동',
+    };
+
+    const newSQL = replaceString(sql, map);
+    expect(newSQL).toEqual("select * from member where name='홍길동'");
+  });
+
+  test('APIController.mappingRequestData', () => {
+    const sql = "select ${field} from ${table} where name='${name}'";
+    const controller = new APIController();
+    controller.queryItem = { type: QueryType.API, query: sql };
+    const mappingQuery = controller.mappingRequestData(controller.queryItem.query, { field: '*', table: 'member', name: 'Hannah' });
+    expect(mappingQuery).toEqual("select * from member where name='Hannah'");
+  });
+
+  test('sql injection', () => {
+    const sql = "select ${field} from ${table} where name='${name}'";
+    const controller = new APIController();
+    controller.queryItem = { type: QueryType.API, query: sql };
+    const mappingQuery = controller.mappingRequestData(controller.queryItem.query, { field: '*', table: 'member', name: "';drop table member;--" });
+    expect(mappingQuery).toEqual("select * from member where name='';drop table member;--'");
+    expect(hasSql(mappingQuery)).toEqual(true);
   });
 });
